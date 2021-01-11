9ad8a2770c0c6cff111322b59bf98f0249a9dcb2
Kendallbrown1021 <kendall.darnel.brown@gmail.com>
2021-01-11 15:38:44 -0500

    broken homepage inso smaller components, finished styling menu item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Directory } from "../../components/directory/directory.component";
+
+import "./homepage.styles.css";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
